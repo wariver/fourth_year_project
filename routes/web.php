52e34e52495dc80c497d08093e80ade42a22e775
wariver <waruindungu@students.uonbi.ac.ke>
2019-09-13 11:05:11 +0300

    route:list work

--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('cat/{id}', 'HomeController@cat');
 Route::resource('maps', 'MapController')->middleware('auth');
-Route::resource('category', 'CategoryController')->middleware('auth');
 Route::resource('device', 'DevicesController')->middleware('auth');
-Route::resource('canvas', 'CategoryController')->middleware('auth');
 Route::get('/trip', 'HomeController@trip')->name('trip');
-
-
-Route::get('category/media/{icon}', function ($icon) {
-    return Image::make(storage_path().'/app/uploads/category/'.$icon)->response();
-});
-
-Route::post('/map', function (Request $request) {
-    $lat = $request->input('lat');
-    $long = $request->input('long');
-    $location = ["lat"=>$lat, "long"=>$long];
-    event(new SendLocation($location));
-    return response()->json(['status'=>'success', 'data'=>$location]);
-});
